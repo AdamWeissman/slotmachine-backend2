f3b2f255717b7b4127a3784bfa2767ec553938d7
AdamWeissman <adam@comedytragedyepic.com>
2020-07-18 13:00:16 -0400

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#home'
+
+  get '/signup' => 'users#new'
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  get '/logout' => 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
+
 end
