17405e0bf9c7c7c04d3cc3ce7cfb60de03d0f413
AdamWeissman <adam@comedytragedyepic.com>
2020-07-18 12:54:00 -0400

    model basics and db corrected

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,2 +1,11 @@
 class Bank < ApplicationRecord
+  def change
+    create_table :banks do |t|
+      t.integer :total
+
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
 end
