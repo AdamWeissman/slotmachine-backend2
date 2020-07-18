f990dc45bdd893d264b2509db395fffac08d5e35
AdamWeissman <adam@comedytragedyepic.com>
2020-07-18 13:31:04 -0400

    backend becomign a mess

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,12 +1,20 @@
 class BanksController < ApplicationController
 
   def new
+    if logged_in?
+      @bank = @user.banks.new
+    end
   end
 
   def create
+    if logged_in?
+      @bank = @user.banks.new(params[:total])
+      @bank.save
+    end
   end
   
   def update
+    @bank = params[:total]
   end
 
   def show
