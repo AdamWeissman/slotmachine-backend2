45ad6a450b5eac192506e2328b6bb7232c6a0b71
AdamWeissman <adam@comedytragedyepic.com>
2020-07-18 13:23:28 -0400

    application helper and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+
+  before_action :require_login
+
+  private
+    def check_for_logged_in
+      redirect_to '/' if !logged_in?
+    end
+
+    def preset_user
+      @user = current_user
+    end
+
 end
